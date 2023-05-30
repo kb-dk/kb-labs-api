630e826deeb2708658de60d25ca02ea41bfb2497
Victor Harbo Johnston <vhol@kb.dk>
2023-05-30 11:23:54 +0200

    Add URLS to metadata objects

--- src/main/java/dk/kb/labsapi/metadataFormats/FullPageMetadata.java
+++ src/main/java/dk/kb/labsapi/metadataFormats/FullPageMetadata.java
@@ -1,6 +1,10 @@
 package dk.kb.labsapi.metadataFormats;
 
+import dk.kb.labsapi.config.ServiceConfig;
 import org.apache.solr.client.solrj.response.QueryResponse;
+
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Object containing metadata about a full page from a newspaper.
@@ -10,19 +14,43 @@ public class FullPageMetadata extends BasicMetadata {
     private String pageUUID;
 
 
-    public FullPageMetadata(String pageUUID, Long pageWidth, Long pageHeight){
+
+    public FullPageMetadata(String pageUUID, Long pageWidth, Long pageHeight) throws IOException {
         String realUUID = pageUUID.replace("doms_aviser_page:uuid:", "");
         this.pageUUID = realUUID;
         this.pageHeight = pageHeight;
         this.pageWidth = pageWidth;
+        this.imageURL = createFullPageLink();
     }
 
     public void setPageUUID(String pageUUID) {
         this.pageUUID = pageUUID;
     }
 
-
     public String getPageUUID() {
         return pageUUID;
+    }
+
+    @Override
+    public void setImageURL(String imageURL) throws IOException {
+        this.imageURL = createFullPageLink();
+    }
+
+
+    // Helper methods
+
+    /**
+     * Construct link to pages from metadata.
+     * @return a URL to the page described in the input metadata.
+     */
+    public URL createFullPageLink() throws IOException {
+        String baseURL = ServiceConfig.getConfig().getString("labsapi.aviser.imageserver.url");
+        String baseParams = "&CVT=jpeg";
+        String pageUuid = this.pageUUID + ".jp2";
+        String prePageUuid = "/" + pageUuid.charAt(0) + "/" + pageUuid.charAt(1) + "/" + pageUuid.charAt(2) + "/" + pageUuid.charAt(3) + "/";
+        String region = "&RGN=1,1,1,1";
+
+        return new URL(baseURL+prePageUuid+pageUuid+baseParams);
+
     }
 }
--- src/main/java/dk/kb/labsapi/metadataFormats/IllustrationMetadata.java
+++ src/main/java/dk/kb/labsapi/metadataFormats/IllustrationMetadata.java
@@ -1,9 +1,13 @@
 package dk.kb.labsapi.metadataFormats;
 
+import dk.kb.labsapi.config.ServiceConfig;
 import dk.kb.util.webservice.exception.InternalServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -42,7 +46,7 @@ public class IllustrationMetadata extends BasicMetadata {
      * @param pageWidth of the  entire page, where the illustration is present.
      * @param pageHeight of the  entire page, where the illustration is present.
      */
-    public IllustrationMetadata(String illustrationString, String pageUUID, long pageWidth, long pageHeight){
+    public IllustrationMetadata(String illustrationString, String pageUUID, long pageWidth, long pageHeight) throws IOException {
         Matcher m = singleIllustrationPattern.matcher(illustrationString);
         if (m.matches()){
             this.id = m.group(1);
@@ -57,6 +61,7 @@ public class IllustrationMetadata extends BasicMetadata {
         this.pageUUID = pageUUID;
         this.pageHeight = (long) convertPixelsToInch1200((double) pageHeight);
         this.pageWidth = (long) convertPixelsToInch1200((double) pageWidth);
+        this.imageURL = createIllustrationLink();
     }
 
     // Getters
@@ -76,6 +81,13 @@ public class IllustrationMetadata extends BasicMetadata {
         return h;
     }
 
+    @Override
+    public void setImageURL(String imageURL) throws IOException {
+        this.imageURL = createIllustrationLink();
+    }
+
+    // Helper methods
+
     /**
      * Convert values from pixel to <a href="https://www.leadtools.com/help/sdk/v22/dh/ft/altoxmlmeasurementunit.html">inch1200</a> using known DPI.
      * @param pixelValue to convert to inch1200 value.
@@ -85,4 +97,57 @@ public class IllustrationMetadata extends BasicMetadata {
         final int DPI = 300;
         return pixelValue / DPI * 1200.0;
     }
+
+    /**
+     * Convert input <a href="https://www.leadtools.com/help/sdk/v22/dh/ft/altoxmlmeasurementunit.html">inch1200</a> value to corrosponding pixel value.
+     * @param inch1200Value to convert.
+     * @return pixel representation of input value.
+     */
+    private double convertInch1200ToPixels(double inch1200Value){
+        final int DPI = 300;
+        return inch1200Value * DPI / 1200.0;
+    }
+
+    /**
+     * Construct link to illustration from metadata.
+     * @param ill is a class containing metadata for an illustration.
+     * @return a URL to the illustration described in the input metadata.
+     */
+    public URL createIllustrationLink() throws IOException {
+        String baseURL = ServiceConfig.getConfig().getString("labsapi.aviser.imageserver.url");
+        String baseParams = "&CVT=jpeg";
+        String pageUuid = this.pageUUID + ".jp2";
+        String prePageUuid = "/" + pageUuid.charAt(0) + "/" + pageUuid.charAt(1) + "/" + pageUuid.charAt(2) + "/" + pageUuid.charAt(3) + "/";
+        String region = calculateIllustrationRegion(this.x, this.y, this.w, this.h, this.pageWidth, this.pageHeight);
+
+        return new URL(baseURL+prePageUuid+pageUuid+region+baseParams);
+
+    }
+
+    /**
+     * Calculate X & W coordinates, width and height for region parameter. Converts input <a href="https://www.leadtools.com/help/sdk/v22/dh/ft/altoxmlmeasurementunit.html">inch1200</a> values to fractions of image size.
+     * The image server containing the images uses the <a href="https://iipimage.sourceforge.io/documentation/protocol/">Internet Imaging Protocol</a>.
+     * @param x coordinate for individual illustration.
+     * @param y coordinate for individual illustration.
+     * @param w represents the width of the individual illustration.
+     * @param h represents the height of the individual illustration.
+     * @param width of page where the illustration is found.
+     * @param height of page where the illustration is found.
+     * @return a region string that is ready to be added to an IIP query.
+     */
+    public String calculateIllustrationRegion(double x, double y, double w, double h, double width, double height){
+        // Fraction calculation from: https://math.hws.edu/graphicsbook/c2/s1.html
+        // newX = newLeft + ((oldX - oldLeft) / (oldRight - oldLeft)) * (newRight - newLeft))
+        // newY = newTop + ((oldY - oldTop) / (oldBottom - oldTop)) * (newBottom - newTop)
+        // The one used here is simplified because it has to return a fraction between 0 and 1.
+        double calculatedX = x / width;
+        double calculatedY = y / height;
+        double calculatedW = w / width;
+        double calculatedH = h / height;
+
+        double widthInPixels = convertInch1200ToPixels(w);
+
+        return String.format(Locale.ROOT, "&WID=%d&RGN=%1.5f,%1.5f,%1.5f,%1.5f", (int) widthInPixels, calculatedX, calculatedY, calculatedW, calculatedH);
+    }
+
 }
