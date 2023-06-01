2d49f189a8c8c77f7ef905745bc8b98b383568b6
Victor Harbo Johnston <vhol@kb.dk>
2023-06-01 12:58:54 +0200

    add lplace to query constructor

--- src/main/java/dk/kb/labsapi/QueryConstructor.java
+++ src/main/java/dk/kb/labsapi/QueryConstructor.java
@@ -5,11 +5,22 @@ import java.util.StringJoiner;
 
 public class QueryConstructor {
 
-    public static String constructQuery(List<String> text, String booleanOperator, Integer startTime, Integer endTime, List<String> familyId, List<String> lplace){
+    /**
+     * Construct a Solr query from input arguments.
+     * @param text that is to be searched for.
+     * @param booleanOperator defines which boolean operator to use e.g. AND, OR, NOT.
+     * @param startYear is the earliest boundary for the query. Boundaries are inclusive.
+     * @param endYear is the latest boundary for the query. Boundaries are inclusive.
+     * @param familyId limits the query to specific newspapers.
+     * @param lplace limits the query to specific publication locations.
+     * @return a string representation of a valid solr query constructed from the inputted parameters.
+     */
+    public static String constructQuery(List<String> text, String booleanOperator, Integer startYear, Integer endYear, List<String> familyId, List<String> lplace){
         String query;
         String textQuaries = "";
         String py = null;
         String familyIdString = null;
+        String lplaceString = null;
 
         if (booleanOperator == null){
             booleanOperator = " AND ";
@@ -22,27 +33,31 @@ public class QueryConstructor {
         }
 
         // How should we handle values that are out of range? Should we correct them or let them be created here and then corrected when used in other endpoints?
-        if (!(startTime == null)){
-            py = "py:[" + startTime + " TO " + endTime + "]";
+        // TODO: Handle startYear and endYear on their own.
+        if (!(startYear == null) && !(endYear == null)){
+            py = "py:[" + startYear + " TO " + endYear+ "]";
+        }
+        if (!(startYear == null) && endYear == null){
+            py = "py:[" + startYear + " TO *]";
+        }
+        if (!(endYear == null) && startYear == null){
+            py = "py:[* TO " + endYear + "]";
         }
 
+        // Transform familyIDs to query
         if (!(familyId == null || familyId.size() == 0)){
-            if (familyId.size() == 1){
-                familyIdString = "familyId:" + familyId.get(0);
-            } else {
-                familyIdString = "familyId:(";
-                for (String id : familyId){
-                    familyIdString = familyIdString.concat(id + " OR ");
-                }
-                familyIdString = familyIdString.substring(0, familyIdString.length() - 4);
-                familyIdString = familyIdString.concat(")");
-            }
+            familyIdString = createFilter(familyId, "familyId");
+        }
+
+        // Transform lplaces to query
+        if (!(lplace == null || lplace.size() == 0)){
+            lplaceString = createFilter(lplace, "lplace");
         }
 
         // Construct query from values
-        query = String.join(booleanOperator, textQuaries, py, familyIdString);
+        query = String.join(booleanOperator, textQuaries, py, familyIdString, lplaceString);
 
-        // Remove AND nulls from query
+        // Remove 'AND nulls' from query
         if (query.contains(booleanOperator + "null")){
             query = query.replaceAll(booleanOperator + "null", "");
         }
@@ -55,5 +70,26 @@ public class QueryConstructor {
         query = query.trim();
 
         return query;
+    }
+
+    /**
+     * Create a filter to use as part of solr query.
+     * @param parameter to create filter from. This list contains one or more values that are to be used in the filter.
+     * @param filterName defines the field to filter on.
+     * @return string containing filter.
+     */
+    private static String createFilter(List<String> parameter, String filterName){
+        String output;
+        if (parameter.size() == 1){
+            output = filterName + ":" + parameter.get(0);
+        } else {
+            output = filterName + ":(";
+            for (String place : parameter){
+                output = output.concat(place + " OR ");
+            }
+            output = output.substring(0, output.length() - 4);
+            output = output.concat(")");
+        }
+        return output;
     }
 }
--- src/test/java/dk/kb/labsapi/QueryConstructorTest.java
+++ src/test/java/dk/kb/labsapi/QueryConstructorTest.java
@@ -40,19 +40,35 @@ public class QueryConstructorTest {
         Assertions.assertEquals(correctString, query);
     }
 
-    // TODO: Tests for timestamps
+    // Tests for timestamps
     @Test
-    public void testTimestamps(){
-        List<String> text = new ArrayList<>(Arrays.asList("hest"));
-        String correctString = "hest AND py:[1740 TO 1750]";
-        String query = QueryConstructor.constructQuery(text, null, 1740, 1750, null, null);
+    public void testStartYear(){
+        List<String> text = new ArrayList<>(List.of("hest"));
+        String correctString = "hest AND py:[1740 TO *]";
+        String query = QueryConstructor.constructQuery(text, null, 1740, null, null, null);
         Assertions.assertEquals(correctString, query);
     }
 
-    // TODO: Tests for familyId
+    @Test
+    public void testEndYear(){
+        List<String> text = new ArrayList<>(List.of("hest"));
+        String correctString = "hest AND py:[* TO 1840]";
+        String query = QueryConstructor.constructQuery(text, null, null, 1840, null, null);
+        Assertions.assertEquals(correctString, query);
+    }
+
+    @Test
+    public void testBothYears(){
+        List<String> text = new ArrayList<>(List.of("hest"));
+        String correctString = "hest AND py:[1790 TO 1840]";
+        String query = QueryConstructor.constructQuery(text, null, 1790, 1840, null, null);
+        Assertions.assertEquals(correctString, query);
+    }
+
+    // Tests for familyId
     @Test
     public void testSingleFamilyId(){
-        List<String> familyId = new ArrayList<>(Arrays.asList("testAvis"));
+        List<String> familyId = new ArrayList<>(List.of("testAvis"));
         String correctString = "familyId:testAvis";
         String query = QueryConstructor.constructQuery(null, null, null, null, familyId, null);
         Assertions.assertEquals(correctString, query);
@@ -66,6 +82,31 @@ public class QueryConstructorTest {
         Assertions.assertEquals(correctString, query);
     }
 
-    // TODO: Tests for lplace
+    // Tests for lplace
+    @Test
+    public void testSingleLplace(){
+        List<String> lplace = new ArrayList<>(List.of("aarhus"));
+        String correctString = "lplace:aarhus";
+        String query = QueryConstructor.constructQuery(null, null, null, null, null, lplace);
+        Assertions.assertEquals(correctString, query);
+    }
+
+    @Test
+    public void testMultipleLplace(){
+        List<String> lplace = new ArrayList<>(Arrays.asList("aarhus", "aalborg", "viborg"));
+        String correctString = "lplace:(aarhus OR aalborg OR viborg)";
+        String query = QueryConstructor.constructQuery(null, null, null, null, null, lplace);
+        Assertions.assertEquals(correctString, query);
+    }
+
+    @Test
+    public void testFamilyIdAndLplace(){
+        List<String> familyId = new ArrayList<>(Arrays.asList("testAvis", "andenAvis", "tredjeAvis"));
+        List<String> lplace = new ArrayList<>(Arrays.asList("aarhus", "aalborg", "viborg"));
+        String correctString = "familyId:(testAvis OR andenAvis OR tredjeAvis) AND lplace:(aarhus OR aalborg OR viborg)";
+
+        String query = QueryConstructor.constructQuery(null, null, null, null, familyId, lplace);
+        Assertions.assertEquals(correctString, query);
+    }
 
 }
